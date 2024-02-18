ddff99048373613c8173e08a2e67d15abfa628bf
Punit Arani <punitsai36@gmail.com>
2024-02-17 21:52:19 -0800

    Add search_web() function

--- althea/nlp/search.py
+++ althea/nlp/search.py
@@ -3,6 +3,22 @@
 from .client import cohere
 
 
+async def search_web(question: str) -> str:
+    """Search the internet for the answer to a question."""
+
+    prediction = await cohere.chat(
+        message=question,
+        preamble_override="You are a scientific research assistant. You answer questions concisely and accurately.",
+        model="command",
+        temperature=0.25,
+        stream=False,
+        citation_quality="accurate",
+        connectors=[{"id": "web-search"}],
+        documents=[],
+    )
+    return prediction.text
+
+
 async def search_wiki(question: str) -> str:
     """Search Wikipedia for the answer to a question."""
 
