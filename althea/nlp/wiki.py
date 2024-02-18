3c0f6188a48638f4c3e5fc555d1d76f5a4dd0270
Punit Arani <punitsai36@gmail.com>
2024-02-17 19:48:17 -0800

    Override wiki search preamble

---
+++ althea/nlp/wiki.py
@@ -0,0 +1,21 @@
+"""althea/nlp/wiki.py"""
+
+from .client import cohere
+
+
+async def search_wiki(question: str) -> str:
+    """Search Wikipedia for the answer to a question."""
+
+    prediction = await cohere.chat(
+        message=question,
+        preamble_override="You are a scientific research assistant. You answer questions concisely and accurately.",
+        model="command-light",
+        temperature=0.25,
+        stream=False,
+        citation_quality="accurate",
+        connectors=[
+            {"id": "web-search", "options": {"site": "https://www.wikipedia.org/"}}
+        ],
+        documents=[],
+    )
+    return prediction.text
