44e6deaaaf4c0b4aa91b1f74284a875346bcce2f
Punit Arani <punitsai36@gmail.com>
2024-02-17 21:09:47 -0800

    Improve prompts and output formatting

--- althea/nlp/hallu.py
+++ althea/nlp/hallu.py
@@ -1,5 +1,7 @@
 """althea/nlp/hallu.py"""
 
+import json
+
 from .client import openai
 
 HALLUCINATE_SYSTEM_PROMPT = """
@@ -10,7 +12,10 @@ You have a summary of the project and need to come up with ideas for the experim
 You need very out-of-the-box ideas that are not constrained by the current state of technology.
 You need to come up with ideas that are not limited by current scientific understanding.
 
+Each idea should be very clear and should not require any further explanation or context.
+
 List 5 ideas for experiments that could be conducted to test the hypothesis.
+List the ideas in JSON format with a single root key "ideas" and a list of strings as the value.
 """.strip()
 
 
@@ -24,9 +29,10 @@ async def hallucinate_ideas(summary: str, direction: str) -> list[str]:
             {"role": "user", "content": summary},
             {"role": "user", "content": direction},
         ],
+        response_format={"type": "json_object"},
         temperature=1,
     )
-    return response.choices[0].message.content
+    return json.loads(response.choices[0].message.content).get("ideas", [])
 
 
 BREAKDOWN_SYSTEM_PROMPT = """
@@ -34,6 +40,16 @@ You are a scientist working on a research project.
 You are trying to break down a complex scientific research idea into simpler components.
 You need to break down the idea into smaller, more manageable parts.
 You need to understand the different aspects of the idea and how they relate to each other.
+
+All the steps should be exploratory research through previous experiments and literature review.
+Do not include any steps that requires conducting new experiments or collecting new data.
+It should be a logical progression of steps that build on each other and lead to a conclusion.
+
+Each step should be very clear and should not require any further explanation or context.
+The step should be very specific and should not be too broad or general.
+
+List the different steps in logical order in JSON format.
+It should have a single root key "steps" and a list of strings as the value.
 """.strip()
 
 
@@ -46,6 +62,7 @@ async def breakdown_idea(idea: str) -> list[str]:
             {"role": "system", "content": BREAKDOWN_SYSTEM_PROMPT},
             {"role": "user", "content": idea},
         ],
+        response_format={"type": "json_object"},
         temperature=0,
     )
-    return response.choices[0].message.content
+    return json.loads(response.choices[0].message.content).get("steps", [])
